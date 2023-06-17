a740af2fe88ef219cfbc81458c119cd6e93c1de3
Prince-Hamza <princehamzi.mine@gmail.com>
2023-06-17 20:32:21 +0500

    img path

--- controllers/ShareController.js
+++ controllers/ShareController.js
@@ -1,10 +1,17 @@
 
-const share = async (req, res) => {    
+const share = async (req, res) => {
     res.set('Content-Type', 'text/html')
     const { title, description, image } = req.query
 
     if (!title || !description || !image) return res.status(400).send(Buffer.from(`<h2> title , description and image are required </h2`))
 
+    console.log(`title :: ${title},  desc:${description} img:${image} `)
+
+    let first = image.substring(0, image.indexOf('/o/') + 3)
+    let second = image.substring(image.indexOf('/o/') + 3, image.indexOf('?')).split('/').join('%2F')
+    let third = image.substring(image.indexOf('?'), image.length)
+    let imgPath = first + second + third
+    console.log(`first :: ${first}, second : ${second}, third : ${third} `)
 
     return res.status(200).send(Buffer.from(`         
         <head>
@@ -13,10 +20,10 @@ const share = async (req, res) => {
            <meta property="og:type" content="article" />
            <meta property="og:title" content=${title} />
            <meta property="og:description" content=${description} />
-           <meta property="og:image" content=${image} />
+           <meta property="og:image" content=${imgPath} />
         </head>
         <body>
-         ${image}
+       <h1>  path : ${imgPath}</h1>
         </body>
    `))
 }
