ea0c79e721347e13b4cb1717d600602fbd43a420
Prince-Hamza <princehamzi.mine@gmail.com>
2023-06-19 13:38:24 +0500

    ok

--- controllers/ShareController.js
+++ controllers/ShareController.js
@@ -3,20 +3,18 @@ const share = async (req, res) => {
 
   res.set('Content-Type', 'text/html')
 
-  const { title, description, imageUrl } = req.query
+  const { title, description, imageUrl, city, date } = req.query
 
   console.log(`title :: ${title},  desc:${description} imageUrl:${imageUrl} `)
 
-  if (!title || !description || !imageUrl) return res.status(400).send(Buffer.from(`<h2> title , description and imageUrl are required </h2`))
-
-  // let imagePath = `https://firebasestorage.googleapis.com/v0/b/my-first-project-ce24e.appspot.com/o/pinpoint%2Fimage%2Fimage_${imageId}?alt=media&token=${imageToken}`
+  if (!title || !description || !imageUrl || !city || !date) return res.status(400).send(Buffer.from(`<h2> title,description,imageUrl,city and date are required </h2`))
 
 
   return res.status(200).send(Buffer.from(
     `
     <title>  ${title}  </title>
-    <meta content="${description}" name="Description">
-    <meta property="og:description" content="${description}">
+    <meta content=""${city} \n ${date} \n ${description}"" name="Description">
+    <meta property="og:description" content="${city} \n ${date} \n ${description}">
 
     <meta content="initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no" name="viewport"> 
     <meta content="chrome=1" http-equiv="X-UA-Compatible"> 
@@ -27,7 +25,7 @@ const share = async (req, res) => {
     <meta content="${imageUrl}" itemprop="image">
     <meta content="${imageUrl}" property="og:image"> 
     <meta content="256" property="og:image:width"> <meta content="256" property="og:image:height"> 
-    <meta content="${description}" itemprop="description"> 
+    <meta content="${city} \n ${date} \n ${description}" itemprop="description"> 
     <meta content="${title}" property="og:site_name">
     <meta content="summary" name="twitter:card"
   `
